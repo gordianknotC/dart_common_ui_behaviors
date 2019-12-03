8bf2039086804f5a5d2b1da13a7ba23d960b77e3
gordianknot <gordianknot.app@gmail.com>
2019-12-03 18:58:47 +0800

    [v] add behavior of SingleGroupAware // untested [V] add tests forSingleGroupAware [v] add scroll show hide aware

--- lib/src/behaviors_base.dart
+++ lib/src/behaviors_base.dart
@@ -3,5 +3,6 @@
 export "group_aware/singleGroupAware.dart";
 export "pick_aware/pickableBloC.dart";
 export "progress_aware/singleProgressAware.dart";
+export "scroll_aware/scroll_show_hide_aware.dart";
 
 
